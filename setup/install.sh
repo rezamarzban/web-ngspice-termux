52f31f279cd073c45a1a39f50ee541ecf7d3b23d
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2025-09-25 13:34:52 +0330

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Update and upgrade packages
+# Update and upgrade packages non-interactively
 pkg update && pkg upgrade -y
 
-# Install required packages
-pkg install wget openssl ngspice -y
+# Install required packages non-interactively, forcing to keep old config files
+apt install -y -o Dpkg::Options::="--force-confold" wget openssl ngspice
 
 # Download server.sh script
 wget https://raw.githubusercontent.com/rezamarzban/web-ngspice-termux/refs/heads/main/setup/server.sh
@@ -18,8 +18,8 @@ chmod +x server.sh
 # Download index.php
 wget https://raw.githubusercontent.com/rezamarzban/web-ngspice-termux/refs/heads/main/index.php
 
-# Make index.php 755 permission 
-chmod -c 755 index.php
+# Set index.php to 755 permission (note: -c is verbose, but -v for verbose if needed; removing -c as it's not standard for chmod)
+chmod 755 index.php
 
 # Copy index.php to nginx html directory
 cp index.php /data/data/com.termux/files/usr/share/nginx/html
